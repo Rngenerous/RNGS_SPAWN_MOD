756b09f7f2019727dfe17cd084ca6c1041d9ec91
Rngenerous <maxwhiteqwertyuio@gmail.com>
2024-01-05 05:50:48 +1100

    added config.boxmax

--- mod.ts
+++ mod.ts
@@ -7,10 +7,17 @@ import { LocationController } from "@spt-aki/controllers/LocationController";
 
 
 
+db.___.___.____= this.modConfig."maxBotCapasonline;
+db.___.___.____= this.modConfig."maxBotCaplow;
+db.___.___.____= this.modConfig."maxBotCapmed;
+db.___.___.____= this.modConfig."maxBotCaphigh;
+db.___.___.____= this.modConfig."maxBotCapHorde;
 
-
+switch BotmaxSettings = 
 switch controllerSettings = this.modConfig
 
 const locationGenerator = container.resolve<LocationGenerator>("LocationGenerator");
 const location
 const modConfig =
+
+module.exports = { mod: new RngSpawnMod() }
