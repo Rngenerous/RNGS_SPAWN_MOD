7a10566c15e04fe4d2af3c93e684c3637e4ced84
Rngenerous <maxwhiteqwertyuio@gmail.com>
2024-01-05 05:39:57 +1100

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,16 @@
+//
+import { DependencyContainer } from "tsyringe";
+import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { ILocations } from "@spt-aki/models/spt/server/ILocations";
+import { LocationController } from "@spt-aki/controllers/LocationController";
+
+
+
+
+
+switch controllerSettings = this.modConfig
+
+const locationGenerator = container.resolve<LocationGenerator>("LocationGenerator");
+const location
+const modConfig =
