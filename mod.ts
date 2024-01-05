5e9be8d5d7d495d01109c22f1d2b3aad7f85794a
Rngenerous <152802828+Rngenerous@users.noreply.github.com>
2024-01-05 16:25:48 +1100

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -7,11 +7,14 @@ import { LocationController } from "@spt-aki/controllers/LocationController";
 
 
 
-db.___.___.____= this.modConfig."maxBotCapasonline;
-db.___.___.____= this.modConfig."maxBotCaplow;
-db.___.___.____= this.modConfig."maxBotCapmed;
-db.___.___.____= this.modConfig."maxBotCaphigh;
-db.___.___.____= this.modConfig."maxBotCapHorde;
+db.___.___.____= this.modConfig.maxBotCapasonline;
+db.___.___.____= this.modConfig.maxBotCaplow;
+db.___.___.____= this.modConfig.maxBotCapmed;
+db.___.___.____= this.modConfig.maxBotCaphigh;
+db.___.___.____= this.modConfig.maxBotCapHorde;
+db.___.___.____= this.modConfig.FactoryIsExperemental;
+db.___.___.____= this.modConfig.IsFps+;
+db.___.___.____= this.modConfig.IsVanillaBoss;
 
 switch BotmaxSettings = 
 switch controllerSettings = this.modConfig
@@ -26,13 +29,14 @@ function Update() {
   if(Input.z(KeyCode)
   script = GetComponent("GC.buffer"); 
   script = GetComponent("GC.Collect"); 
-  script = GetComponent("GCbuffer"); 
+  script = GetComponent("GC.Alloc"); 
   script.enabled = false;
   }
   {
   
   if(Input.GetKeyDown(KeyCode.P))
-  script = GetComponent("PlatformerCharacter2D"); 
+  script = GetComponent(""); 
   script.enabled = false;
-  }
+  },
+db.___.___.____= this.modConfig.GCscript.enabled;
 module.exports = { mod: new RngSpawnMod() }
